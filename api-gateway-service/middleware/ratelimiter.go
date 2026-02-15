a4fbd6ecdd2d75d42cbde6c81ba7d3f6e4d0d40f
Rudranil Bhattacharya <rudranilms7@gmail.com>
2026-02-15 13:45:38 +0530

    implemented rate limit logic

---
+++ api-gateway-service/middleware/ratelimiter.go
@@ -0,0 +1,85 @@
+package middleware
+
+import (
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type client struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+
+type RateLimiter struct {
+	mu      sync.Mutex
+	clients map[string]*client
+	r       rate.Limit
+	b       int
+}
+
+func NewRateLimiter(r rate.Limit, b int) *RateLimiter {
+	return &RateLimiter{
+		clients: make(map[string]*client),
+		r:       r,
+		b:       b,
+	}
+}
+
+func getIP(r *http.Request) string {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+	return ip
+}
+
+func (rl *RateLimiter) getLimiter(ip string) *rate.Limiter {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	c, exists := rl.clients[ip]
+	if !exists {
+		limiter := rate.NewLimiter(rl.r, rl.b)
+		rl.clients[ip] = &client{
+			limiter:  limiter,
+			lastSeen: time.Now(),
+		}
+		return limiter
+	}
+
+	c.lastSeen = time.Now()
+	return c.limiter
+}
+
+func (rl *RateLimiter) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		ip := getIP(r)
+		limiter := rl.getLimiter(ip)
+
+		if !limiter.Allow() {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (rl *RateLimiter) CleanUp() {
+	for {
+		time.Sleep(time.Minute)
+
+		rl.mu.Lock()
+		for ip, c := range rl.clients {
+			if time.Since(c.lastSeen) > 3*time.Minute {
+				delete(rl.clients, ip)
+			}
+		}
+		rl.mu.Unlock()
+	}
+}
--- go.mod
+++ go.mod
@@ -23,4 +23,5 @@ require (
 	golang.org/x/crypto v0.48.0
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
+	golang.org/x/time v0.14.0
 )
